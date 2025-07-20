4d2c8a1c5c5b0dd8f4cd5f35288f7d683100c137
Elfinbolt <uddhavnaik05@gmail.com>
2025-07-20 12:31:09 +0530

    initial commit

---
+++ src/Components/About/About.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import './About.css'
+import theme_pattern from '../../assets/theme_pattern.svg'
+import profile_img from '../../assets/about_profile.svg'
+
+const About = () => {
+  return (
+    <div className='about'>
+      <div className="about-title">
+        <h1>About me</h1>
+        <img src={theme_pattern} alt="" />
+      </div>
+      <div className="about-sections">
+        <div className="about-left">
+          <img src={profile_img} alt="" />
+        </div>
+        <div className="about-right">
+          <div className="about-para">
+            <p>’m a passionate Frontend Developer focused on crafting clean, interactive, and responsive web interfaces that deliver great user experiences.</p>
+            <p>I build sleek, fast, and accessible web interfaces. With a frontend focus, I enjoy creating intuitive user experiences and pixel-perfect designs.</p>
+          </div>
+          <div className="about-skills">
+            <div className="about-skill"><p>HTML & CSS</p><hr style={{width:"50%"}}/></div>
+            <div className="about-skill"><p>Javascript</p><hr style={{width:"70%"}}/></div>
+            <div className="about-skill"><p>React Js</p><hr style={{width:"60%"}}/></div>
+            <div className="about-skill"><p>Next Js</p><hr style={{width:"50%"}}/></div>
+          </div>
+        </div>
+      </div>
+      <div className="about-achievements">
+        <div className="about-achievement">
+          <h1>5+</h1>
+          <p>YEARS OF EXPERIENCE</p>
+        </div>
+        <hr />
+        <div className="about-achievement">
+          <h1>90+</h1>
+          <p>PROJECTS COMPLETED</p>
+        </div>
+        <hr />
+        <div className="about-achievement">
+          <h1>15+</h1>
+          <p>HAPPY CLIENTS</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default About
---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Hero.css'
+import profile_img from '../../assets/profile_img.svg'
+
+const Hero = () => {
+  return (
+    <div className='hero'>
+      <img src={profile_img} alt="" />
+      <h1><span>I'am taylor durden,</span>frontend developer based in USA.</h1>
+      <p>Frontend Developer skilled in building responsive and user-friendly web interfaces using HTML, CSS, and JavaScript.</p>
+      <div className="hero-action">
+        <div className="hero-connect">Connect with me</div>
+        <div className="hero-resume">My resume</div>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
